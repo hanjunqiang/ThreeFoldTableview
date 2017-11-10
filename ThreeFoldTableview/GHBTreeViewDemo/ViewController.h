9d9a6b5ce2e8e1b8a985094a2b4733e1f010bec4
Mr.han <hanjunqiang@users.noreply.github.com>
2017-11-10 15:19:29 +0800

    tableview三级折叠列表
    tableview三级折叠列表

---
+++ ThreeFoldTableview/GHBTreeViewDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  GHBTreeViewDemo
+//
+//  Created by 123 on 15/11/12.
+//  Copyright © 2015年 haibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+
+@end
+
